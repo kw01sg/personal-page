ba9c05fecd00d51deb98e317e00158758095dd01
Kian Boon Koh <kw01sg@gmail.com>
2018-06-17 22:20:58 +0800

    implement placeholder component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaceholderComponent
   ],
   imports: [
     BrowserModule,
