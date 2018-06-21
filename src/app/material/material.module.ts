4f5ac878cc29869cf31348ebea20b8e0e9f91692
Kian Boon Koh <kianboonkoh@gmail.com>
2018-06-21 17:08:55 +0800

    implement experience component

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule
   ],
   exports: [
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule
   ]
 })
 export class MaterialModule { }
