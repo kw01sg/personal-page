7840de4fcf7a9956c0fc273a3c64cd31517a609b
Kian Boon Koh <kianboonkoh@gmail.com>
2018-07-04 11:46:26 +0800

    implement page nav in personal info page

---
+++ src/app/personal-info/personal-info-nav-links.ts
@@ -0,0 +1,12 @@
+import { PageNavItem } from "../page-header/page-nav/page-nav-item.model";
+
+export const PERSONAL_INFO_NAV_LINKS: PageNavItem[] = [{
+    displayName: 'Education',
+    id: 'education'
+}, {
+    displayName: 'Technical Skills',
+    id: 'technical-skills'
+}, {
+    displayName: 'Interests', 
+    id: 'interests'
+}];
--- src/app/personal-info/personal-info.component.ts
+++ src/app/personal-info/personal-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PageNavItem } from '../page-header/page-nav/page-nav-item.model';
+import { PERSONAL_INFO_NAV_LINKS } from './personal-info-nav-links';
 
 @Component({
   selector: 'app-personal-info',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-info.component.css', '../shared/styles.css']
 })
 export class PersonalInfoComponent implements OnInit {
+  personalInfoNavItems: PageNavItem[];
 
   constructor() { }
 
   ngOnInit() {
+    this.personalInfoNavItems = PERSONAL_INFO_NAV_LINKS;
   }
 
 }
